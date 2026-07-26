edd39bbbfc5adf9cad75341d07c5632ed7559b24
Lima152010 <lucaslbr1a2b@gmail.com>
2026-07-25 21:35:25 -0300

    docs: atualiza configuração do Firebase e README

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,20 +1,16 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-const admin = require("firebase-admin");
-
-admin.initializeApp({
-    credential: admin.credential.cert({
+initializeApp({
+    credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
         privateKey: process.env.FIREBASE_PRIVATE_KEY
             .replace(/\\n/g, "\n")
     })
 });
 
-const auth = admin.auth();
+const auth = getAuth();
 
 module.exports = {
     auth
